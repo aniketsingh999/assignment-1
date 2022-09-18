9d0a4c52e576340421830b396021ef7d87b35d96
Aniket Kumar <anikets679@gmail.com>
2022-09-18 15:46:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+require('express-async-errors');
+
+
+
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const connectDB = require('./db/connect');
+
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json());
+app.use(morgan('dev'))
+
+app.get('/', (req, res)=> {
+  res.send('hello world')
+})
+
+const port = process.env.PORT || 5000;
+
+const start = async() => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, ()=> console.log(`Server is listening at port ${port}`))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+start()
